7c1913b16735b0d91c8d80799646003a29efd9cd
JoujonikiAsa2 <joujonikiasaroy@gmail.com>
2023-12-04 20:27:08 +0600

    Added some changes on header and footer

--- src/SharedComponents/Footer/Footer.jsx
+++ src/SharedComponents/Footer/Footer.jsx
@@ -5,13 +5,19 @@ const Footer = () => {
         <div>
             <div className='mt-12 bg-slate-200'>
                 <footer className="flex lg:flex-row md:flex-row flex-col text-base-content lg:justify-between md:justify-between justify-center gap-4 lg:px-28 py-6">
-                    <aside className='flex flex-col gap-3 lg:text-left text-center'>
-                        <div className='flex lg:justify-start justify-center gap-3'>
-                            <img src={logo} alt="" className='w-10 h-10' />
-                            <h2 className='text-xl font-bold'><span className='text-[#21B573]'>Wisdom</span> <span className='text-[#0645B1]'>Wave</span></h2>
+                    <aside className='flex flex-col justify-center items-center gap-3 lg:text-left text-center'>
+                        <div className='flex flex-col justify-center items-center gap-3'>
+                            <div className='flex gap-2 justify-center items-center'>
+                                <img src={logo} alt="" className='lg:w-10 lg:h-10 w-6 h-6' />
+                                <div>
+                                    <h2 className='text-lg font-bold'><span className='text-[#21B573]'>Wisdom</span> <span className='text-[#0645B1]'>Wave</span></h2>
+                                </div>
+                            </div>
+                            <p className='max-w-[350px]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, aut obcaecati nam voluptatum nesciunt exercitationem.</p>
+                            <div className=''>
+                                <button className='btn btn-outline text-[#0645B1] w-32'>Join</button>
+                            </div>
                         </div>
-                        <p className='max-w-[350px]'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Veritatis, aut obcaecati nam voluptatum nesciunt exercitationem.</p>
-                        <button className='btn btn-outline text-[#0645B1]'>Join</button>
                     </aside>
                     <nav className='flex flex-col lg:text-left text-center'>
                         <header className="text-lg font-bold text-black pb-3">Company</header>
@@ -44,10 +50,8 @@ const Footer = () => {
                     </nav>
                 </footer>
                 <footer className='flex flex-col justify-center items-center py-3'>
-                    <div className='flex flex-col justify-center items-center'>
-                        <div className='divider flex justify-center w-[300px] lg:w-[1000px] md:[w-1000px] '></div>
-                    </div>
-                    <div className='pb-3'>
+                    <div className='divider'></div>
+                    <div className='pb-3 text-center'>
                         <p>Copyright © 2023 - All right reserved by WisdomWave Ltd</p>
                     </div>
                 </footer>
